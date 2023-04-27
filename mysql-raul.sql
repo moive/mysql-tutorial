ee5311d0b270b4d1676bee4b8b4833e1aacb8690
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-04-26 22:05:28 -0500

    exerices sql

--- mysql-raul.sql
+++ mysql-raul.sql
@@ -24,3 +24,16 @@ SELECT * FROM ACTOR;
 SELECT actor_id AS Id, first_name AS Name, last_name AS LastName FROM actor LIMIT 10;
 
 SELECT city_id AS ID, city, country_id AS 'COUNTRY ID', last_update AS 'LAST UPDATE' FROM CITY LIMIT 20;
+
+-- distinct
+SELECT DISTINCT(store_id) FROM CUSTOMER; -- 2
+
+SELECT DISTINCT(customer_id) FROM payment; // 599
+
+-- order by
+SELECT * FROM country ORDER BY country DESC;
+
+-- min and max value amount table payment
+select distinct(amount) from payment order by amount asc;
+select min(amount) from payment;
+select max(amount) from payment;
