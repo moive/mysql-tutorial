bb55210dc7beae01d01dd6e4cce707537a22eeba
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-14 17:28:17 -0500

    BETWEEN

--- mysql-raul.sql
+++ mysql-raul.sql
@@ -72,3 +72,18 @@ SELECT * FROM category WHERE name NOT IN ('Action','Animation','Children');
 
 SELECT * FROM film_text WHERE title IN ('ZORRO ARK', 'VIRGIN DAISY', 'UNITED PILOT');
 SELECT * FROM city WHERE city IN ('Chiayi', 'Dongying', 'Fukuyama', 'Kilis');
+
+
+-- USE BETWEEN
+SELECT * FROM rental WHERE (customer_id BETWEEN 300 AND 350) AND staff_id = 1;
+SELECT * FROM payment WHERE amount BETWEEN 3 AND 5;
+
+-- USE NOT
+SELECT * FROM payment WHERE amount NOT BETWEEN 3 AND 5;
+
+-- EXERCICES
+SELECT * FROM payment WHERE (amount BETWEEN 2.99 AND 4.99) AND (staff_id = 2) AND (customer_id BETWEEN 1 AND 2);
+SELECT * FROM payment WHERE (amount BETWEEN 2.99 AND 4.99) AND (staff_id = 2) AND customer_id IN(1,2);
+
+SELECT * FROM address WHERE city_id BETWEEN 300 AND 350;
+SELECT * FROM film WHERE (rental_rate BETWEEN 0.99 AND 2.99) AND (length <= 50) AND (replacement_cost < 20);
